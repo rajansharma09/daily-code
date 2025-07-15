084fd48abf2bc01032eb1eb843da86d38bceb0ee
rajansharma09 <rajansharma200606@gmail.com>
2025-07-15 21:36:54 +0530

    Add files via upload

---
+++ cwh_09_presedance.java
@@ -0,0 +1,16 @@
+public class cwh_09_presedance {
+    public static void main(String [] args){
+    //int a =5*2-18/2;
+    /*=10-18/2
+      =10-9
+      =1
+     */
+    // * & / have mor precedance,left to rigth assosiativity.
+   // System.out.println(a);
+    int a= 20;
+    int b=10;
+    int c=8;
+    System.out.println(b*b-4*a*c/2*a);
+    }
+    
+}
